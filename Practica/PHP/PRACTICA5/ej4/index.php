bf1820d374e8ce7d17e498cefc86a111b90f3818
Joshua Acciarri <acciarrijoshua@gmail.com>
2022-05-21 20:53:19 -0300

    Ej 4 de sesion

---
+++ Practica/PHP/PRACTICA5/ej4/index.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+
+<?php
+if (!isset($_SESSION["contador"])){
+    $_SESSION["contador"] = 1;
+    }   else{
+    $_SESSION["contador"]++;
+} 
+?>
+
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav navbar-dark bg-dark">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contacto.php">Contacto</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="consultas.php">Consultas</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+    <h1>Este es el home</h1>
+    <?php
+    echo "<h3> Has visitado un total de ". ($_SESSION["contador"]). " páginas </h3>";
+    ?> 
+</div>
+
+</body>
+</html>
