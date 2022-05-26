fbdcf7c870742df6c900f3d9cf2b6608cbf1929b
Joshua <acciarrijoshua@gmail.com>
2022-05-25 21:26:41 -0300

    Partials creado para reutilizar el mismo header y footer una y otra vez

---
+++ Sistema Consultas/partials/footer.php
@@ -0,0 +1,14 @@
+<!-- 
+    Separando el footer para reutilizarlo en todas las views y cambiarlo una sola vez
+    y en un solo lugar en caso de que haya algun cambio 
+!-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+    integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+    crossorigin="anonymous"></script>
+  
+</body>
+</html>
---
+++ Sistema Consultas/partials/header.php
@@ -0,0 +1,18 @@
+<!-- 
+    Separando el footer para reutilizarlo en todas las views y cambiarlo una sola vez
+    y en un solo lugar en caso de que haya algun cambio 
+!-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    
+    <title>Sistema de Consultas</title>
+</head>
+<body>
