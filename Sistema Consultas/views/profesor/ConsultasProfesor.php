6a9d2fbaa6b03be7b3651077bded1cda3adf2aa5
juanchicruzz <59696948+juanchicruzz@users.noreply.github.com>
2022-06-23 16:25:05 -0300

    arreglo

--- Sistema Consultas/views/profesor/ConsultasProfesor.php
+++ Sistema Consultas/views/profesor/ConsultasProfesor.php
@@ -24,12 +24,6 @@ Conflicts
 
 $consultas = $consultaRepository->getConsultasByProfesor($_SESSION["id"]);
 
-$consultas = $consultaRepository->getConsultasByProfesor($_SESSION["id"]);
-$consultas = $consultaRepository->getConsultasByProfesor($_SESSION["id"]);
-$consultas = $consultaRepository->getConsultasByProfesor($_SESSION["id"]);
-$consultas = $consultaRepository->getConsultasByProfesor($_SESSION["id"]);
-$consultas = $consultaRepository->getConsultasByProfesor($_SESSION["id"]);
-
 ?>
 
 <script type="text/javascript" charset="utf8" src="../tablas/crearTablaConsultasProfesor.js"></script>
